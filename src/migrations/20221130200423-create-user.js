728bff1ee6a7c4f80299ffa68be5c37907cad2a7
Rafael Souza <souza_rafael.97@outlook.com>
2022-11-30 17:27:56 -0300

    Criação do users migration

---
+++ src/migrations/20221130200423-create-user.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ src/models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+  });
+  return user;
+};
